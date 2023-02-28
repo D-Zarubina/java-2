1ad8aa227096246ccb16331ad712a6ffeb42fc2f
Daria <dasha.cp@mail.ru>
2023-02-28 22:50:17 +0300

    Commit_1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 15720;
+        int oneMile = 20;
+        int bonus = amount / oneMile;
+
+        System.out.println("Спасибо за покупку! Вам будет начислено " + bonus + " миль(я/ей)");
+    }
+}
